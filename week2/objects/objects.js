011839cf07186eff73cb8585e19e092d8f34aa2c
maddyasma <maddyasma@me.com>
2026-01-29 23:49:46 -0600

    created objects file

---
+++ week2/objects/objects.js
@@ -0,0 +1,15 @@
+let myGames = [
+{ name: "Animal Crossing", players: "1", type: "video game", rating: "9/10"};
+{ name: "Balder's Gate", players: "4", type: "video game", rating: "8/10"};
+{ name: "Fall Out New Vegas", players: "1", type: "video game", rating: "7/10"};
+]
+console.log(myGames);
+
+let pickOne = prompt("I have 3 games in my collection. Pick a number between 1 and 3 and I'll tell you about that game.");
+let promptAnswer = Number(pickOne)-1;
+window.alert ("you got" + 
+    myGames[promptResponse].name + ", a" + 
+    myGames[promptAnswer].players + " player game. It's a " + 
+    myGames[promptAnswer].type + "which I gave a rating of " + 
+    myGames[promptAnswer].rating + "."
+);
