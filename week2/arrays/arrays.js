04c9442f93977a8c9da1a1f8d03b3573fc176875
maddyasma <maddyasma@me.com>
2026-01-29 22:44:42 -0600

    Created files and added arrays

---
+++ week2/arrays/arrays.js
@@ -0,0 +1,27 @@
+let myArr = ['Moulin Rouge','La La Land','Pride & Prejudice','Mamma Mia'];
+//let myArr.unshift() = prompt("What is your favorite movie?");
+console.log(myArr);
+
+let newMovie = window.prompt("What is your favorite movie?")
+myArr.push(newMovie);
+
+
+// let myOtherArray = new Array();
+// myOtherArray.push('star wars');
+// myOtherArray.push('raiders');
+// //myArr.pop(); //removes last item
+// //myArr.shift();//removes first item
+// //myArr.unshift('james');//adds item to beginning
+// console.log(myArr);
+// console.log(myOtherArray);
+// console.log(myOtherArray[0]); //read single item
+// console.log(myArr[1]);
+// console.log(myArr.indexOf('betty'));
+// const bettyIdx = myArr.indexOf ('betty');
+// console.log(bettyIdx);
+// if (bettyIdx > -1); {
+//     console.log(myArr[bettyIdx]);
+// }
+// myArr.splice(bettyIdx,1);
+// console.log(myArr);
+// console.log(myArr.length);
