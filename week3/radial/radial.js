4d97751222e8390f1b5e152bb85cc447408cdc48
maddyasma <maddyasma@me.com>
2026-02-12 22:01:49 -0600

    file created and edited

--- week3/radial/radial.js
+++ week3/radial/radial.js
@@ -4,23 +4,28 @@ function setup(){
     createCanvas(600,600);
     background('lightblue');
     angleMode(DEGREES);
-
 }
+
 function makeArm(rotateBy){
     let alt = Math.round(rotateBy / 360);
     noFill ();
     stroke('#FAEDB7');
     strokeWeight(2);
-    rect(0, 0 + alt, 800 / alt);
+    rect(0, 0 + alt, 600 / alt);
 }
-
+//
 function draw(){
-    translate(600,600);
+    translate(300,300); //changes position
     rotate(rotateBy);
     makeArm(rotateBy);
-    rotateBy += 5;
+    rotateBy += 10;
 }
 function mousePressed(){
+   if (isLooping()){
     noLoop();
+    } else {
+    loop();
+    }
 }
-window.alert("Click to pause the spirograph :-)")
+
+// window.alert("Click to pause the spirograph :-)")
