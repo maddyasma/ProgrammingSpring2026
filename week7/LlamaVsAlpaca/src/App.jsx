46a1918d759ac284ce60093dba2e0db17ba8f539
maddyasma <maddyasma@me.com>
2026-03-05 22:41:40 -0600

    fixed tags

--- week7/LlamaVsAlpaca/src/App.jsx
+++ week7/LlamaVsAlpaca/src/App.jsx
@@ -1,20 +1,27 @@
 import Camelid from "./components/Camelid";
+import llamaImage from "./components/img/llama.png";
+import alpacaImage from "./components/img/alpaca.png";
 
 function App() {
 const camelidData = [
-  {name: "Llama", hex: "#cc8899", desc: "a color with a hilarious name"},
-  {name: "Alpaca", hex: "#2b9c2b", desc: "the color of plants"},
+  {name: "Llama",
+  img: <img src={llamaImage} />, 
+  desc: "Llamas are related to camels, and like camels, are used to carry goods over long distances"},
+
+  {name: "Alpaca", 
+  img: <img src={alpacaImage} />, 
+  desc: "Alpacas are small, domesticated South American camelids and are raised primarily for their soft, hypoallergenic, and warm fleece"},
 ]
   return(
     <div>
       <h2>Camelids</h2>
       <Camelid name={camelidData[0].name} 
-      desc={colorData[0].desc} 
-      hex={colorData[0].hex}/>
+      desc={camelidData[0].desc} 
+      img={camelidData[0].img}/>
 
       <Camelid name={camelidData[1].name} 
-      desc={colorData[1].desc} 
-      hex={colorData[1].hex}/>
+      desc={camelidData[1].desc} 
+      img={camelidData[1].img}/>
 
     </div>
   )
--- week7/LlamaVsAlpaca/src/components/Camelid.jsx
+++ week7/LlamaVsAlpaca/src/components/Camelid.jsx
@@ -1,15 +1,11 @@
 import "./Camelid.css";
-function Camelid({name, desc, hex}){
+function Camelid({name, desc, img}){
     console.log(name);
     return (
         <div className="camelidCard">
             <h1>{name}</h1>
             <p>{desc}</p>
-            <div style={{
-                width: "25px",
-                height: "25px",
-                backgroundColor: hex,
-            }}></div>
+            <div>{img} </div>
         </div>
     );
 
