9bd561259ab2153d4c368fd672bd146460752e9c
maddyasma <maddyasma@me.com>
2026-03-15 23:28:09 -0500

    updated colors and fonts

--- week8/assignment/src/masthead/Masthead.jsx
+++ week8/assignment/src/masthead/Masthead.jsx
@@ -1,5 +1,6 @@
 import mastheadImg from "../assets/masthead.png";
 import "./Masthead.css";
+import winkyRough from "https://fonts.googleapis.com/css2?family=Winky+Rough:ital,wght@0,300..900;1,300..900&display=swap";
 export default function Masthead() {
     return (
         <div className="masthead">
