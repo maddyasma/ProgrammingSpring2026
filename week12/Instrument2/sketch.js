f96c3b3899a38619283ada55314eed7f2bc49b26
maddyasma <maddyasma@me.com>
2026-04-19 20:04:03 -0500

    Built water ripples and assigned sounds

---
+++ week12/Instrument2/sketch.js
@@ -0,0 +1,77 @@
+let cols;
+let rows;
+let current; 
+let previous;
+
+let sounds = [];
+
+function preload () {
+  sounds = [
+    loadSound('BUILD01short.wav'),
+    loadSound('BUILD02short.wav'),
+    loadSound('BUILD03short.wav'),
+    loadSound('BUILD04short.wav'),
+    loadSound('BUILD05short.wav'),
+    loadSound('BUILD06short.wav'),
+    loadSound('BUILD07short.wav'),
+  ];
+}
+let dampening = 0.99;
+
+function setup() {
+  pixelDensity(1);
+  createCanvas(1700, 920);
+  cols = width;
+  rows = height;
+ 
+
+  current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+}
+
+function mouseClicked() {
+  previous[mouseX][mouseY] = 6000;
+
+  let rate = map(mouseX, 0, width, 0.5, 5); // speed/pitch
+  let s = sounds[5];
+  s.rate(rate);
+  s.play();
+}
+
+function draw() {
+  background(0);
+
+  loadPixels();
+  for (let i = 1; i < cols - 1; i++) {
+    for (let j = 1; j < rows - 1; j++) {
+      current[i][j] =
+        (previous[i - 1][j] +
+          previous[i + 1][j] +
+          previous[i][j - 1] +
+          previous[i][j + 1]) /
+          2 -
+        current[i][j];
+      current[i][j] = current[i][j] * dampening;
+      
+      
+      let index = (i + j * cols) * 4;
+      let val = current[i][j];
+
+      //background color
+      let r = 10;
+      let g = 40;
+      let b = 50;
+      
+      let brightness = Math.max(0, val);
+
+      pixels[index + 0] = r + brightness;
+      pixels[index + 1] = g + brightness;
+      pixels[index + 2] = b + brightness;
+    }
+  }
+  updatePixels();
+
+  let temp = previous;
+  previous = current;
+  current = temp;
+}
